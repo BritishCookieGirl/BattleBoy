aa66405c0100e6855147d36b10dae842f4b2f811
Rein <rein@rein-online.org>
2013-10-23 18:48:12 -0400

    Added test level with initial movement scripts

---
+++ Flamboyant Battle Boy/Assets/_Scripts/Force2D.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Force2D : MonoBehaviour
+{
+    private Transform trans;
+    private Rigidbody body;
+    //historical x-position
+    private float x;
+
+    public void Start()
+    {
+        //cashe components for good performance
+        trans = transform;
+        body = rigidbody;
+
+        x = trans.position.x;
+    }
+
+    public void FixedUpdate()
+    {
+        //reset x-position
+        Vector3 pos = trans.position;
+        pos.x = x;
+        trans.position = pos;
+
+        //clear Y and Z rotation
+        Vector3 dir = trans.localRotation * Vector3.forward;
+        dir.x = 0;
+        trans.localRotation = Quaternion.LookRotation(dir, -Vector3.Cross(Vector3.right, dir));
+
+        //clear X-velocity
+        Vector3 vel = body.velocity;
+        vel.x = 0;
+        body.velocity = vel;
+
+        //clear Y and Z angular velocity
+        body.angularVelocity = new Vector3(body.angularVelocity.x, 0, 0);
+    }
+}
---
+++ Flamboyant Battle Boy/Assets/_Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    //@script AddComponentMenu("Camera-Control/Smooth Look At");
+
+    public Transform target;
+
+    public float damping = 6.0f;
+    public bool enableCameraPivot = true;
+    public bool smoothPivot = true;
+
+    //Movement speed in units/sec
+    public float moveSpeed = 1.0f;
+
+    //Transforms to act as start and end markers for the journey
+    private Vector3 startPoint;
+    private Vector3 endPoint;
+
+    //Time when the movement started
+    private float startTime;
+
+    //Total distance between the markers
+    private float journeyLength;
+
+    private Vector3 cameraOffset;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cameraOffset = new Vector3(0f, 1f, 0f);
+
+        // Make the rigid body not change rotation
+        if (rigidbody)
+            rigidbody.freezeRotation = true;
+
+        //Keep a note of the time the movement started.
+        startTime = Time.time;
+
+        startPoint = transform.position;
+        endPoint = new Vector3(target.position.x, target.position.y, -target.position.z) + cameraOffset;
+
+        //Calculates the journey length;
+        journeyLength = Vector3.Distance(startPoint, endPoint);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Follows the target position like with a spring
+
+        if ((transform.position == target.position + cameraOffset) && Time.time - startTime > 1000)
+        {
+            startPoint = transform.position;
+            startTime = Time.time;
+        }
+
+        endPoint = new Vector3(target.position.x, target.position.y, -target.position.z) + cameraOffset;
+        journeyLength = Vector3.Distance(startPoint, endPoint);
+
+        //Distance moved = time * speed
+        float distCovered = (Time.time - startTime) * moveSpeed;
+
+        //Fraction of journey completed = current distance divided by total distance.
+        float fracJourney = distCovered / journeyLength;
+
+        //Set our position as a fraction of the distance between the markers.
+        transform.position = Vector3.Lerp(startPoint, endPoint, fracJourney);
+	}
+
+    void LateUpdate()
+    {
+        if (enableCameraPivot)
+        {
+            if (smoothPivot)
+            {
+                // Look at and dampen the rotation
+                Quaternion rotation = Quaternion.LookRotation(target.position + cameraOffset - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+            }
+            else
+            {
+                // Just lookat
+                transform.LookAt(target.position + cameraOffset);
+            }
+        }
+    }
+}
