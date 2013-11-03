e740c7d871f80722c5981a565300cef08eb2fd67
BabyMaybe <Eric_Varela@mac.com>
2013-11-03 15:34:59 -0500

    Started Unlock Store
    Basic icons with placeholder sounds and mouse effects

---
+++ Flamboyant_Battle_Boy/Assets/_Scripts/Item Scripts/UnlockableItem.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class UnlockableItem : MonoBehaviour {
+	
+	public Texture unlockedTexture;
+	public Texture lockedTexture;
+	public AudioClip hoverSound;
+	public AudioClip selectedSound;
+	public bool unlocked;
+	private Color normalColor = new Color(0.6f,0.6f,0.6f);
+	private Color hoverColor = new Color(0.75f,0.75f,0.75f);
+	private Color activeColor = new Color(1.0f,1.0f,1.0f);
+	
+	// Use this for initialization
+	void Start () {
+		renderer.material.color = normalColor;
+		if (unlocked) {
+			renderer.material.mainTexture = unlockedTexture;
+		} else {
+			renderer.material.mainTexture = lockedTexture;
+		}
+		
+		audio.clip = hoverSound;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseEnter() {
+		//renderer.material.mainTexture = unlockedTexture;
+		renderer.material.color = hoverColor;
+		audio.Play();
+	}
+	
+	void OnMouseExit() {
+		//renderer.material.mainTexture = lockedTexture;
+		renderer.material.color = normalColor;
+	}
+	
+	void OnMouseDown() {
+		renderer.material.color = activeColor;
+		audio.clip = selectedSound;
+		audio.Play();
+	}
+	
+	void OnMouseUp() {
+		renderer.material.color = normalColor;
+		audio.clip = hoverSound;
+	}
+}
