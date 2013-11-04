eb7c5256695dc7a3a5031391a1b045e179178b1e
BabyMaybe <Eric_Varela@mac.com>
2013-11-04 01:35:59 -0500

    Unlock Store Item Window
    Took waaaaaaaaaaaay too long getting this to work :(
    But it updates the window when you click things now. Still need to make
    the buttons work right, and add default behavior or make mystery boxes
    unclickable

---
+++ Flamboyant_Battle_Boy/Assets/_Scripts/Item Scripts/StoreManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoreManager : MonoBehaviour {
+	
+	private float height =250;
+	private float width = 400;
+	
+	public UnlockableItem displayItem;
+	
+	public GUIStyle itemNameStyle;
+	public GUIStyle itemPointStyle;
+	public GUIStyle itemEffectStyle;
+	public GUIStyle itemDescriptionStyle;
+	public GUIStyle dividerStyle;
+	public GUIStyle buttonStyle;
+
+	public Rect windowRect;
+	
+	void Start() {
+		
+		windowRect = new Rect((Screen.width/2 - width/2), 20, width, height);
+		print ("Box Height = " + height);
+		print ("Box Width = " + width);
+	}
+    
+	void OnGUI() {
+        windowRect = GUI.Window(0, windowRect, ItemWindow, "");
+    }
+	
+    void ItemWindow(int windowID) {
+		GUI.Label(new Rect(15,15,130,130),displayItem.unlockedTexture);
+        GUI.Label(new Rect(165,10,200,100),displayItem.itemName,itemNameStyle);
+		GUI.Label(new Rect(150,35,200,5), "************************", dividerStyle);
+        GUI.Label(new Rect(155,50,200,100),displayItem.PointString(),itemPointStyle);
+		GUI.Label(new Rect(155,75,200,100),displayItem.itemEffect, itemEffectStyle);
+		
+		if (GUI.Button(new Rect(150,110,100,30),"Buy",buttonStyle)){
+			BuyClicked();
+		}
+		if (GUI.Button(new Rect(260,110,130,30),"Cancel",buttonStyle)){
+			CancelClicked();
+		}
+		
+		GUI.Label(new Rect(10,155,200,5), "**************************************", dividerStyle);
+		GUI.Label(new Rect(20,175,370,100),displayItem.itemDescription, itemDescriptionStyle);
+    }
+	
+	void BuyClicked() {
+		print ("item purchased");
+		//destroy window
+		//adjust points
+		//adjust abilities
+	}
+	
+	void CancelClicked() {
+		print ("item not purchased");
+	}
+	
+	public void UpdateIcon(UnlockableItem display) {
+		displayItem = display.GetComponent<UnlockableItem>();
+	}
+}
--- Flamboyant_Battle_Boy/Assets/_Scripts/Item Scripts/UnlockableItem.cs
+++ Flamboyant_Battle_Boy/Assets/_Scripts/Item Scripts/UnlockableItem.cs
@@ -4,18 +4,30 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class UnlockableItem : MonoBehaviour {
 	
+	public string itemName;
+	public string itemEffect;
+	public string itemDescription;
+	public int itemCost;
+	
 	public Texture unlockedTexture;
 	public Texture lockedTexture;
+	
 	public AudioClip hoverSound;
 	public AudioClip selectedSound;
+	
 	public bool unlocked;
+	
+	private StoreManager store;
+		
 	private Color normalColor = new Color(0.6f,0.6f,0.6f);
 	private Color hoverColor = new Color(0.75f,0.75f,0.75f);
 	private Color activeColor = new Color(1.0f,1.0f,1.0f);
 	
+
 	// Use this for initialization
 	void Start () {
 		renderer.material.color = normalColor;
+		store = transform.parent.parent.GetComponent<StoreManager>();
 		if (unlocked) {
 			renderer.material.mainTexture = unlockedTexture;
 		} else {
@@ -35,6 +47,7 @@ public class UnlockableItem : MonoBehaviour {
 		//renderer.material.mainTexture = unlockedTexture;
 		renderer.material.color = hoverColor;
 		audio.Play();
+		
 	}
 	
 	void OnMouseExit() {
@@ -43,13 +56,20 @@ public class UnlockableItem : MonoBehaviour {
 	}
 	
 	void OnMouseDown() {
+		print (this.GetComponent<UnlockableItem>());
+		print (store);
 		renderer.material.color = activeColor;
 		audio.clip = selectedSound;
+		store.UpdateIcon(this.GetComponent<UnlockableItem>());
 		audio.Play();
 	}
 	
 	void OnMouseUp() {
 		renderer.material.color = normalColor;
 		audio.clip = hoverSound;
+	}
+	
+	public string PointString() {
+		return itemCost.ToString() + " pts";
 	}
 }
