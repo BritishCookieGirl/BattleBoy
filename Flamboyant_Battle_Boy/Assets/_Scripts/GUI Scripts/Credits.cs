188ae6293bab5ba4150ea1df1c43f27b55867b8b
BabyMaybe <Eric_varela@mac.com>
2013-11-12 21:36:52 -0500

    Fixed Credits Button

---
+++ Flamboyant_Battle_Boy/Assets/_Scripts/GUI Scripts/Credits.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+	
+	public GUIStyle buttonStyle;
+	public GUIStyle bTextStyle;
+	public GUIStyle bTextShaddowStyle;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI() {
+		
+		if (GUI.Button (new Rect (520, 450, 180, 75), "", buttonStyle)) {
+			GameManager.TriggerStoreClosed();
+		}
+		
+		GUI.Label(new Rect(545,445,225,75), "Back",bTextShaddowStyle);
+		GUI.Label(new Rect(545,445,225,75), "Back",bTextStyle);
+	}
+}
